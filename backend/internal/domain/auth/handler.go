d56e4f74e2de317f01df5ca53e7ae15ab15d09a8
Satria Manggala Putra Pratama <satriampp@proton.me>
2026-06-01 19:09:56 +0700

    feat(auth): improve error response

---
+++ backend/internal/domain/auth/handler.go
@@ -0,0 +1,16 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/stringptr/SiGizi/backend/internal/httputils"
+	"github.com/stringptr/SiGizi/backend/internal/jwtutils"
+)
+
+type Handler interface {
+	Register(ctx context.Context, input *httputils.APIRequestInput[*RegisterRequest]) (*httputils.APIResponseOutput[any], error)
+	Me(ctx context.Context, input *struct{}) (*httputils.APIResponseOutput[*jwtutils.Claim], error)
+	Login(ctx context.Context, input *httputils.APIRequestInput[*LoginRequest]) (*AuthOutput, error)
+	Refresh(ctx context.Context, input *struct{}) (*AuthOutput, error)
+	Logout(ctx context.Context, input *struct{}) (*LogoutOutput, error)
+}
--- backend/internal/domain/auth/service.go
+++ backend/internal/domain/auth/service.go
@@ -4,11 +4,12 @@ import (
 	"context"
 
 	"github.com/gofrs/uuid/v5"
+	"github.com/stringptr/SiGizi/backend/internal/errorutils"
 )
 
 type Service interface {
-	Register(ctx context.Context, dataDTO *RegisterRequest) error
-	Login(ctx context.Context, req *LoginRequest, ip string) (*AuthResponse, error)
-	Refresh(ctx context.Context, refreshToken uuid.UUID, ip string) (*AuthResponse, error)
-	Logout(ctx context.Context, refreshToken uuid.UUID) error
+	Register(ctx context.Context, dataDTO *RegisterRequest) *errorutils.Error
+	Login(ctx context.Context, req *LoginRequest, ip string) (*AuthResponse, *errorutils.Error)
+	Refresh(ctx context.Context, refreshToken uuid.UUID, ip string) (*AuthResponse, *errorutils.Error)
+	Logout(ctx context.Context, refreshToken uuid.UUID) *errorutils.Error
 }

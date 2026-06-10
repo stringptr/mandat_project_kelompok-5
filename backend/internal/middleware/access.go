2d8edfd933f5a8bfd8d6ee5df6139dd842a1004b
Satria Manggala Putra Pratama <satriampp@proton.me>
2026-06-10 09:12:32 +0700

    feat(security): init ip and jwt blacklist

--- backend/internal/middleware/access.go
+++ backend/internal/middleware/access.go
@@ -6,11 +6,12 @@ import (
 	"slices"
 
 	"github.com/danielgtaylor/huma/v2"
+	"github.com/stringptr/SiGizi/backend/internal/domain/jwtblacklist"
 	"github.com/stringptr/SiGizi/backend/internal/httputils"
 	"github.com/stringptr/SiGizi/backend/internal/jwtutils"
 )
 
-func AuthAccessMiddleware(api huma.API, jwt *jwtutils.JWT) func(ctx huma.Context, next func(huma.Context)) {
+func AuthAccessMiddleware(api huma.API, jwt *jwtutils.JWT, blacklistRepo jwtblacklist.Repo) func(ctx huma.Context, next func(huma.Context)) {
 	return func(ctx huma.Context, next func(huma.Context)) {
 		cookie, err := httputils.ReadCookie(ctx, "access_token")
 		if err != nil {
@@ -25,8 +26,41 @@ func AuthAccessMiddleware(api huma.API, jwt *jwtutils.JWT) func(ctx huma.Context
 			return
 		}
 
+		if claims.ID != "" {
+			blacklisted, err := blacklistRepo.IsBlacklisted(ctx.Context(), claims.ID)
+			if err == nil && blacklisted {
+				huma.WriteErr(api, ctx, http.StatusUnauthorized, "Sesi telah berakhir. Silahkan login ulang.", nil)
+				return
+			}
+		}
+
 		newCtx := context.WithValue(ctx.Context(), httputils.AccessKey, claims)
 		next(huma.WithContext(ctx, newCtx))
+	}
+}
+
+func AuthRequiredAccessMiddleware(api huma.API, jwt *jwtutils.JWT, blacklistRepo jwtblacklist.Repo) func(ctx huma.Context, next func(huma.Context)) {
+	return func(ctx huma.Context, next func(huma.Context)) {
+		claims := httputils.GetAccessClaim(ctx.Context())
+
+		if claims == nil {
+			huma.WriteErr(api, ctx, http.StatusUnauthorized, "Silahkan login terlebih dahulu.", nil)
+			return
+		}
+
+		next(ctx)
+	}
+}
+
+func NotLoggedInRequiredMiddleware(api huma.API, jwt *jwtutils.JWT, blacklistRepo jwtblacklist.Repo) func(ctx huma.Context, next func(huma.Context)) {
+	return func(ctx huma.Context, next func(huma.Context)) {
+		claims := httputils.GetAccessClaim(ctx.Context())
+
+		if claims == nil {
+			next(ctx)
+		}
+
+		huma.WriteErr(api, ctx, http.StatusUnauthorized, "Anda sudah loginl. Silahkan logout terlebih dahulu.", nil)
 	}
 }
 
